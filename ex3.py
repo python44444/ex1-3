bd96a6dead4a54d9af5c1baf17d5549f8e0dc921
gRaY4449 <onlyforschoolclass378@gmail.com>
2022-11-09 17:00:17 +0900

    update

--- ex3.py
+++ ex3.py
@@ -1,51 +1,8 @@
-# from flask import Flask
-# from PIL import Image
-# from flask import render_template
-# import numpy as np
-
-# from img import make
-
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def button():
-#     return render_template("index.html")
-
-
-# @app.route("/img")
-# def img():
-#     row_data = []
-#     h = np.random.randint(0, 255)
-#     for v in range(256):
-#         row_data.append(h)
-
-#     hue_data = np.tile(row_data, (256, 1))
-#     sat_data = np.transpose(hue_data)
-#     val_data = np.full_like(hue_data, 255)
-#     mat_data = np.stack([hue_data, sat_data, val_data], 2)
-
-#     im = Image.fromarray(np.uint8(mat_data), "HSV")
-#     im_rgb = im.convert("RGB")
-
-#     im_resize = im_rgb.resize((384, 256))
-#     im_resize.save("static/images/out.png")
-#     path = "static/images/out.png"
-
-#     return render_template("color.html", image=path)
-
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", debug=True, port=5000)
-
-
 from flask import Flask
-from PIL import Image
 from flask import render_template
-import numpy as np
 import secrets
 
-from img import make
+from img import make_path, make_img
 
 app = Flask(__name__)
 
@@ -59,22 +16,11 @@ def button():
 
 @app.route("/img")
 def img():
-    row_data = []
-    h = np.random.randint(0, 255)
-    for v in range(256):
-        row_data.append(h)
+    im_rgb = make_img()
 
-    hue_data = np.tile(row_data, (256, 1))
-    sat_data = np.transpose(hue_data)
-    val_data = np.full_like(hue_data, 255)
-    mat_data = np.stack([hue_data, sat_data, val_data], 2)
+    out_path = make_path(im_rgb)
 
-    im = Image.fromarray(np.uint8(mat_data), "HSV")
-    im_rgb = im.convert("RGB")
-
-    outpath = make(im_rgb)
-
-    return render_template("color.html", image=outpath)
+    return render_template("color.html", image=out_path)
 
 
 if __name__ == "__main__":
